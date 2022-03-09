a0b9fc62970a1398e6b6e7e8c1c7543dfa28b04a
martin835 <64438132+martin835@users.noreply.github.com>
2022-03-09 15:21:25 +0100

    fetching and displaying one article works

--- src/views/blog/index.jsx
+++ src/views/blog/index.jsx
@@ -6,30 +6,58 @@ import BlogLike from "../../components/likes/BlogLike";
 import posts from "../../data/posts.json";
 import "./styles.css";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Blog = () => {
-  const [blog, setBlog] = useState({});
+  const [article, setArticle] = useState({});
+
+  const params = useParams();
+
+  useEffect(() => {
+    fetchArticle();
+  }, []);
+
+  const fetchArticle = async () => {
+    console.log(params.id);
+    const apiUrl = process.env.REACT_APP_BE_URL;
+
+    try {
+      let response = await fetch(`${apiUrl}/articles/${params.id}`);
+      if (response.ok) {
+        let data = await response.json();
+        console.log(data);
+        setArticle(data);
+      } else {
+        alert("something went wrong :(");
+        this.setState({
+          isError: true,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="blog-details-root">
       <Container>
-        <Image className="blog-details-cover" src={blog.cover} fluid />
-        <h1 className="blog-details-title">{blog.title}</h1>
+        <Image className="blog-details-cover" src={article.cover} fluid />
+        <h1 className="blog-details-title">{article.title}</h1>
 
         <div className="blog-details-container">
           <div className="blog-details-author">
-            <BlogAuthor {...blog.author} />
+            <BlogAuthor {...article.author} />
           </div>
           <div className="blog-details-info">
-            <div>{blog.createdAt}</div>
-            <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
+            <div>{article.createdAt}</div>
+            {/* <div>{`${article.readTime.value} ${article.readTime.unit} read`}</div> */}
             <div style={{ marginTop: 20 }}>
               <BlogLike defaultLikes={["123"]} onChange={console.log} />
             </div>
           </div>
         </div>
 
-        <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+        <div dangerouslySetInnerHTML={{ __html: article.content }}></div>
       </Container>
     </div>
   );
